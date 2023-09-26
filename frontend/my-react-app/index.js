35ee891a3675c5fc88d3d351180b448086358245
Ben Joselson <benjoselson@gmail.com>
2023-09-26 14:31:56 -0400

    Updated to use ES6 and restructured project

--- frontend/my-react-app/index.js
+++ frontend/my-react-app/index.js
@@ -1,9 +1,9 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 const port = 3001;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World! Index.js');
 });
 
 app.listen(port, () => {
--- frontend/my-react-app/src/App.js
+++ frontend/my-react-app/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Hello World App.js</h1>
     </div>
   );
 }

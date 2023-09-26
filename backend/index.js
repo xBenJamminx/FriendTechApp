1345f80d0e9916de9c83497fec4cb50db21b3dd5
Ben Joselson <benjoselson@gmail.com>
2023-09-26 14:00:29 -0400

    Fixed server log message

--- backend/index.js
+++ backend/index.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log('Server running at http://localhost:${port}/');
+    console.log(`Server running at http://localhost:${port}/`);
 });

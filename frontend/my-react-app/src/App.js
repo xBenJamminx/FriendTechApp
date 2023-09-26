fea9a35f411ea6bdc6f31be42d090cf4304b32d0
Ben Joselson <benjoselson@gmail.com>
2023-09-26 15:19:38 -0400

    Updated table styling

--- frontend/my-react-app/src/App.js
+++ frontend/my-react-app/src/App.js
@@ -6,6 +6,35 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World App.js</h1>
+      <table>
+        <th>
+          Howdy
+        </th>
+        <tr>
+          <td>Helloooo</td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </table>
     </div>
   );
 }
